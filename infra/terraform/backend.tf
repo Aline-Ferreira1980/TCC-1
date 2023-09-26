152ea4da4e26a482256519e12218591cf2298ee8
Gustavo Favini <gfavini@gmail.com>
2023-09-25 23:05:57 -0300

    - tentativa de subir a aplicação na aws via terraform

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -12,8 +12,7 @@ terraform {
     key            = "terraform.tfstate"
     region         = "sa-east-1"
     dynamodb_table = "terraform-state-lock"
-      access_key = "AKIAWIVJRZRRS4CUOVMN"
-    secret_key = "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"
+
   }
 }
 
