640e77ab1b7bdc665e7589dcc411ce68d363e511
Gustavo Favini <gfavini@gmail.com>
2023-10-08 23:58:12 -0300

    - ajuste do cors e dos arquivos de config

--- gestao_agenda_backend/src/main/java/br/uscs/gestao_agenda_backend/infrastructure/security/config/CorsConfig.java
+++ gestao_agenda_backend/src/main/java/br/uscs/gestao_agenda_backend/infrastructure/security/config/CorsConfig.java
@@ -2,18 +2,23 @@ package br.uscs.gestao_agenda_backend.infrastructure.security.config;
 
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+@Configuration
 public class CorsConfig {
+    @Value("${agenda-cesep.frontend-origin}")
+    private String frontEndOrigin;
     @Bean
     public FilterRegistrationBean<CorsFilter> corsFilterBean() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedOrigins(Collections.singletonList(this.frontEndOrigin));
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
 
