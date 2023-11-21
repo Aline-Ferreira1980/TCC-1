aab7818816e9b0a7d2dc6a07c5ce0ae0e2310996
Gustavo Favini <gfavini@gmail.com>
2023-11-20 22:14:36 -0300

    - Ajustes nas lables

--- agenda-ui/src/services/estagiario.py
+++ agenda-ui/src/services/estagiario.py
@@ -8,7 +8,8 @@ class EstagiarioClient(AgendaClient):
     def __init__(self, auth_token):
         super().__init__()
         self.base_url = self.base_url
-        self.update_headers("Authorization", "Bearer " + auth_token)
+        if auth_token:
+            self.update_headers("Authorization", "Bearer " + auth_token)
 
     def get_by_id(self, id):
         return self.get(f"estagiario/{id}")
